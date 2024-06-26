f27bbbed14e7211cc2c40e2ffc3e2c4080d03a3c
Caio Rosa <dev.caiorosa@gmail.com>
2024-06-26 12:35:15 +0100

    chore: Update page layout and add authentication button

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,35 @@
-import { Button } from "@/components/ui/button";
+import { Poppins } from 'next/font/google'
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import LoginButton from '@/components/auth/login-button'
 
-
+const font = Poppins({
+  subsets: ['latin'],
+  weight: ['600'],
+})
 export default function Home() {
   return (
-    <h1>Hello World</h1>
-  );
+    <main className="flex h-full flex-col items-center justify-center bg-secondary-color">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            'text-6xl font-semibold  text-primary-text-light',
+            font.className,
+          )}
+        >
+          Auth
+        </h1>
+        <p className="text-lg text-primary-text-light">
+          A Next.js simple authentication app
+        </p>
+        <div>
+          <LoginButton>
+            <Button variant="destructive" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  )
 }
