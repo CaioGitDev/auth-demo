d914047a2fdf5c4dd5d8f0b87c49b2eddd99a230
Caio Rosa <dev.caiorosa@gmail.com>
2024-06-26 15:00:34 +0100

    chore: Add back button to CardWrapper component
    The CardWrapper component in the auth module has been updated to include a back button. This allows users to navigate back to the previous page.

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,3 +1,18 @@
+'use cliet'
+import { FaGithub } from 'react-icons/fa'
+import { FcGoogle } from 'react-icons/fc'
+import { Button } from '@/components/ui/button'
+
 export function Social() {
-  return <div>social</div>
+  return (
+    <div className="flex items-center w-full gap-x-2">
+      <Button size="lg" className="w-full" onClick={() => {}} variant="outline">
+        <FcGoogle className="w-5 h-5" />
+      </Button>
+
+      <Button size="lg" className="w-full" onClick={() => {}} variant="outline">
+        <FaGithub className="w-5 h-5" />
+      </Button>
+    </div>
+  )
 }
