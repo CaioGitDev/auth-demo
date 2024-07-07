021d4c2e580feeb96527a268f007252dc628c1af
Caio Rosa <dev.caiorosa@gmail.com>
2024-07-07 08:55:10 +0100

    chore: Add .env-sample file with default values

--- auth.config.ts
+++ auth.config.ts
@@ -1,10 +1,18 @@
 import bcrypt from 'bcryptjs'
 import type { NextAuthConfig } from 'next-auth'
 import credentials from 'next-auth/providers/credentials'
+import github from 'next-auth/providers/github'
 import { LoginSchema } from '@/schemas'
 import { getUserByEmail } from '@/data/user'
+import google from 'next-auth/providers/google'
+
 export default {
   providers: [
+    google,
+    github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials)
