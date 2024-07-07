e6a3d2f7956c14216afc6640f7d607737575f174
Caio Rosa <dev.caiorosa@gmail.com>
2024-07-07 07:40:54 +0100

    feat: Add session handling and sign out functionality

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,20 @@
-export default function DashboardPage() {
+import { auth, signOut } from '@/auth'
+
+export default async function DashboardPage() {
+  const session = await auth()
+
   return (
     <div>
       <h1>DashboardPage</h1>
+      <div>{JSON.stringify(session)}</div>
+      <form
+        action={async () => {
+          'use server'
+          await signOut()
+        }}
+      >
+        <button type="submit">Sign Out</button>
+      </form>
     </div>
   )
 }
